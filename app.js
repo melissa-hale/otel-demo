a8d2e3a76fbc67418f0dae1e1f25937bbf58a7a4
Melissa Hale <melissakayh@gmail.com>
2024-03-07 15:13:28 -0600

    traces

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // https://opentelemetry.io/docs/languages/js/getting-started/nodejs/
 const express = require('express');
+const { rollTheDice } = require('./dice.js');
 
 const PORT = parseInt(process.env.PORT || '8080');
 const app = express();
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
 app.get('/rolldice', (req, res) => {
-  res.send(getRandomNumber(1, 6).toString());
+  const rolls = req.query.rolls ? parseInt(req.query.rolls.toString()) : NaN;
+  if (isNaN(rolls)) {
+    res
+      .status(400)
+      .send("Request parameter 'rolls' is missing or not a number.");
+    return;
+  }
+  res.send(JSON.stringify(rollTheDice(rolls, 1, 6)));
 });
 
 app.listen(PORT, () => {
